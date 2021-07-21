d27ff6bb6e55e40b1eaf70c2cc30241fb8cebf44
nhrnhr0 <nhrnhr0@gmail.com>
2021-07-21 11:15:04 +0300

    small improvments to proformence

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function fetch_data() {
 }
 
 function update_html(data) {
-
     update_swiper(data);
     update_swiper2(data);
 }
@@ -35,13 +34,8 @@ function productsToSwiperHtml(products) {
     </div>`
     return html;
 }
-function update_swiper2(data) {
-    if(data == undefined) {
-        console.log('could not update update_swiper2, data is undefined');
-        return;
-    }
-    var products = data.products;
-    html = productsToSwiperHtml(products);
+
+function show_swiper2() {    
     var swiper_settings = {
         loop: true,
         disableOnInteraction: true,
@@ -54,45 +48,42 @@ function update_swiper2(data) {
             reverseDirection:true,
         }
     };
-    document.querySelector('#swiper2_1').innerHTML = html;
-    var textSwiper = new Swiper('#swiper2_1', swiper_settings)
-    /*
-    html1 = productsToSwiperHtml(products.slice(0, products.length/3));
-    html2 = productsToSwiperHtml(products.slice(products.length/3, (products.length/3)*2 ));
-    html3 = productsToSwiperHtml(products.slice((products.length/3)*2 ));
-    document.querySelector('#swiper2_1').innerHTML = html1;
-    document.querySelector('#swiper2_2').innerHTML = html2;
-    document.querySelector('#swiper2_3').innerHTML = html3;
-    tab1Swipers = [];
-    var swiper_settings = {
-        loop: true,
-        disableOnInteraction: true,
-        direction: 'vertical',
-        speed:4000,//4000,
-        spaceBetween: 0,
-        slidesPerView: "3",
-        autoplay: {
-            delay:0,
-        }
-    };
-    var swiper_settings2 = {
-        loop: true,
-        disableOnInteraction: true,
-        direction: 'vertical',
-        speed:4000,//4000,
-        spaceBetween: 0,
-        slidesPerView: "6",
-        autoplay: {
-            delay:0,
-        }
-    };
+    swiper2_1 = new Swiper('#swiper2_1', swiper_settings)
 
-    tab3Swipers = []
-    tab3Swipers.push(new Swiper('#swiper2_1', swiper_settings2));
-    tab3Swipers.push(new Swiper('#swiper2_2', swiper_settings));
-    tab3Swipers.push(new Swiper('#swiper2_3', swiper_settings2));
-    */
 }
+
+const swiper2_1_dom = document.querySelector('#swiper2_1');
+var swiper2_1;
+
+function update_swiper2(data) {
+    if(data == undefined) {
+        console.log('could not update update_swiper2, data is undefined');
+        return;
+    }
+    var products = data.products;
+    shuffleArray(products);
+    html = productsToSwiperHtml(products);
+    swiper2_1_dom.innerHTML = html;
+}
+
+
+function shuffleArray(array) {
+    var currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
 function productsToImageSwiperHtml(products) {
     var html = '<div class="swiper-wrapper">';
@@ -111,18 +102,8 @@ function productsToImageSwiperHtml(products) {
     return html;
 }
 
-function update_swiper(data){
-    if(data == undefined) {
-        console.log('could not update update_swiper2, data is undefined');
-        return;
-    }
-    var products = data.products;
-    html1 = productsToImageSwiperHtml(products);
-    //html2 = productsToImageSwiperHtml(products.slice(products.length/2,products.length));
-    
-    document.querySelector('#swiper1_1').innerHTML = html1;
-    //document.querySelector('#swiper1_2').innerHTML = html2;
-    
+var swiper1_1;
+function show_swiper() {
     var swiper_settings = {
         loop: true,
         disableOnInteraction: true,
@@ -134,33 +115,29 @@ function update_swiper(data){
             delay:0,
             reverseDirection:true,
         }
-      };
-    var swiper_settings2 = {
-        loop: true,
-        disableOnInteraction: true,
-        speed:3000,//4000,
-        spaceBetween: 0,
-        slidesPerView: "5",
-        autoplay: {
-            delay:0,
-            reverseDirection: false,
-        }
-      };
-    tab1Swiper = new Swiper('#swiper1_1', swiper_settings);
-    tab1Swiper = new Swiper('#swiper1_2', swiper_settings2);
-        
+    };
+    swiper1_1 = new Swiper('#swiper1_1', swiper_settings);
+}
+const swiper1_1_dom = document.querySelector('#swiper1_1');
+function update_swiper(data){
+    if(data == undefined) {
+        console.log('could not update update_swiper2, data is undefined');
+        return;
+    }
+    var products = data.products;
+    shuffleArray(products);
+    html1 = productsToImageSwiperHtml(products);
+    swiper1_1_dom.innerHTML = html1;    
 }
 
 
 fetch_data();
-setInterval(fetch_data, 1000);
+setInterval(fetch_data, 20000);
 
 
 const BULLET_SLEEP = 1000;
-
+var bullets = document.querySelectorAll('.bullet');
 setInterval(()=> {
-    var bullets = document.querySelectorAll('.bullet');
-
     for(var i = 0; i < bullets.length; i++) {
         b = bullets[i];
         b.classList.add('animate');
@@ -177,8 +154,8 @@ function tab1Init() {
     update_swiper(current_data)
 }
 
+var vidDom = document.querySelector('.video video');
 function tab2Init() {
-    var vidDom = document.querySelector('.video video');
     vidDom.play();
 }
 
@@ -186,27 +163,35 @@ function tab3Init() {
     update_swiper2(current_data);
 }
 
+function tab1Show() {
+    show_swiper();
+}
 
+function tab2Show() {
+    vidDom.currentTime = 0;
+}
+
+function tab3Show() {
+    show_swiper2();
+}
 
 
 var last_tab_change_stemp;
-const TABS_TIMERS = [40000,50000,40000];//[0,0,50000];////[0,0,50000];//
+const TABS_TIMERS =[40000,50000,40000];// [40000,50000,40000];//[0,0,50000];////[0,0,50000];//
 const tabs = document.querySelectorAll('.tab');
 var current_tab_idx;
 function moveToTab(last, newTab) {
-    tabs[last].classList.remove('active');
+    /*
     tabs[last].classList.add('fadeout');
     setTimeout(()=> {
         tabs[last].classList.remove('fadeout');
     },1500);
-    tabs[newTab].classList.add('active');
+    
     tabs[newTab].classList.add('fadein');
     setTimeout(()=> {
         tabs[newTab].classList.remove('fadein');
-    },1500);
+    },1500);*/
     current_tab_idx = newTab;
-    console.log('move to tab: ', newTab);
-    last_tab_change_stemp =Date.now();
     switch(newTab) {
         case 0:
             tab1Init();
@@ -218,18 +203,34 @@ function moveToTab(last, newTab) {
             tab3Init();
             break;
     }
-    
+    setTimeout(()=>{
+        tabs[last].classList.remove('active');
+        tabs[newTab].classList.add('active');
+        switch(newTab) {
+            case 0:
+                tab1Show();
+                break;
+            case 1:
+                tab2Show();
+                break;
+            case 2:
+                tab3Show();
+                break;
+        }
+    })
 }
 
 function tabs_flow_update() {
-    console.log(last_tab_change_stemp);
+    //console.log(last_tab_change_stemp);
+    const now = Date.now()
     if(last_tab_change_stemp == undefined) {
         moveToTab(0,0);
-        last_tab_change_stemp = Date.now();
+        last_tab_change_stemp = now;
     }
     
-    else if(last_tab_change_stemp + TABS_TIMERS[current_tab_idx] <= Date.now()) {
+    else if(last_tab_change_stemp + TABS_TIMERS[current_tab_idx] <= now) {
         moveToTab(current_tab_idx, (current_tab_idx+1)%TABS_TIMERS.length);
+        last_tab_change_stemp = now;
     }
 }
 
